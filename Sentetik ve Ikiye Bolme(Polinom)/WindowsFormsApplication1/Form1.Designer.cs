f1cf494501879dc9fbeb4a6a6adb5038ceeedc9a
Uğur Oruç <uoruc5@hotmail.com>
2016-03-09 13:15:11 +0200

    Eksikler Eklendi

---
+++ Sentetik ve Ikiye Bolme(Polinom)/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.coz = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // coz
+            // 
+            this.coz.Location = new System.Drawing.Point(12, 12);
+            this.coz.Name = "coz";
+            this.coz.Size = new System.Drawing.Size(121, 49);
+            this.coz.TabIndex = 0;
+            this.coz.Text = "Çöz";
+            this.coz.UseVisualStyleBackColor = true;
+            this.coz.Click += new System.EventHandler(this.coz_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 601);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Polinom Derecesi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(306, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 681);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.coz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button coz;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sentetik ve Ikiye Bolme(Polinom)/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void coz_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 10;
+            dataGridView1.RowCount = 20;
+
+            dataGridView1.Columns[0].HeaderText = "a";
+            dataGridView1.Columns[1].HeaderText = "b";
+            dataGridView1.Columns[2].HeaderText = "c";
+            dataGridView1.Columns[3].HeaderText = "d";
+            dataGridView1.Columns[4].HeaderText = "e";
+            dataGridView1.Columns[5].HeaderText = "f";
+            dataGridView1.Columns[6].HeaderText = "g";
+            dataGridView1.Columns[7].HeaderText = "h";
+            dataGridView1.Columns[8].HeaderText = "i";
+            dataGridView1.Columns[9].HeaderText = "j";
+         
+            double[] a = { 1, -5, -34, -56, 192 };
+            int n = a.GetLength(0);
+            double[] x = new double[n-1]; 
+            for (int i = 0; i < n-1; i++)
+            {
+                x[i] = Newton(0, a);
+                a = Reduce(a, x[i]);
+            }
+
+            /* 
+            int n = Convert.ToInt32(textBox1.Text);
+            double[] a = new double[n + 1];
+            Random r = new Random();
+            for (int i = 0; i < n + 1; i++)
+            {
+                a[i] = Math.Truncate((r.NextDouble() - 0.5) * 10);
+            } 
+            */
+
+        }
+        /* Indirgeme */
+        private double[] Reduce(double[] a,double r)
+        {
+            int n = a.GetLength(0);
+            double[] b = new double[n - 1];
+            b[n - 2] = a[n - 1];
+            for (int i = n-3; i >= 0; i--)
+            {
+                b[i] = a[i + 1] + r *b[i + 1];
+            }
+
+            return b;
+        }
+        /* Kok Bulma */
+        private double Newton(double x0, double[] a)
+        {
+            double eps = 1e-3;
+            double x1 = x0;
+
+            do
+            {
+                x0 = x1;
+                x1 = (x0 - P(x0,a)) / Pt(x0,a);
+
+            } while (Math.Abs(x1-x0) > eps);
+
+            return x1;
+        }
+        /* Polinom Degeri */
+        private double P(double x,double[] a)
+        {
+            int n = a.GetLength(0);
+            double t = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                t += a[i] * Math.Pow(x, i);
+            }
+            return t;
+        }
+        /* Polinom Turev Degeri */
+        private double Pt(double x, double[] a)
+        {
+            int n = a.GetLength(0);
+            double t = 0.0;
+            for (int i = 1; i < n; i++)
+            {
+                t += a[i] * i * Math.Pow(x, i-1);
+            }
+            return t;
+        }
+
+    }
+}
